3b1cb225c8eb7bd8898a7536db4cff50b6f09da8
Sierra Ashford <sierraashford@gmail.com>
2024-01-24 20:11:04 -0500

    Capstone

--- vue/src/views/DoctorReviewsView.vue
+++ vue/src/views/DoctorReviewsView.vue
@@ -1,23 +1,95 @@
 <template>
-    <Navbar :buttonText="navbarButtonText" :buttonDestination="navbarButtonDestination" />
-    <div class="home">
-      <h1> My Reviews</h1>
+  <Navbar :buttonText="navbarButtonText" :buttonDestination="navbarButtonDestination" />
+  <div class="reviews-page-container">
+    <h1>My Reviews</h1>
+    <div class="reviews-list">
+      <div class="review-entry" v-for="review in reviews" :key="review.id">
+        <h3>{{ review.response }}</h3>
+        <div class="star-display">
+          <span v-for="star in 5" :key="star" class="star" :class="{ 'filled': star <= review.rating }">&#9733;</span>
+        </div>
+        <p class="review-excerpt">{{ review.reviewNote }}</p>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import Navbar from '../components/NavBar.vue'
-  export default {
-    components: {
-      Navbar,
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/NavBar.vue'
+import ReviewService from '../services/ReviewService';
+
+export default {
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      reviews: [],
+      doctorId: this.$route.params.doctorId,
+    };
+  },
+  computed: {
+    navbarButtonText() {
+      return "Log Out";
     },
-    computed: {
-      navbarButtonText() {
-        return "Log Out";
-      },
-      navbarButtonDestination() {
-        return "logout";
-      }
+    navbarButtonDestination() {
+      return "logout";
+    }
+  },
+  created() {
+    this.fetchReviews();
+  },
+  methods: {
+    fetchReviews() {
+      console.log(`Fetching reviews for doctor ID: ${this.doctorId}`);
+      ReviewService.getReviewsByDoctorId(this.doctorId)
+        .then(response => {
+          console.log('Reviews fetched:', response.data); 
+          this.reviews = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching reviews:', error);
+        });
     },
-  };
-  </script>
+  }
+};
+</script>
+
+  <style scoped>
+  .reviews-page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1200px;
+  margin: auto;
+  padding: 100px;
+}
+
+.reviews-header {
+  text-align: center;
+}
+
+.reviews-list {
+  max-width: 80%;
+}
+
+.review-entry {
+  background-color: #f4f4f4;
+  padding: 30px;
+  margin-bottom: 20px;
+  border-radius: 5px;
+}
+
+.review-excerpt {
+  margin-bottom: 20px;
+}
+
+.star-display {
+  font-size: 30px;
+  color: #ccc;
+}
+
+.star-display .star.filled {
+  color: #ffd055;
+}
+</style>
